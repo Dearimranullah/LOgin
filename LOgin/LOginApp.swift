7012248039af4357c8ddae6a37179138d7c2c965
Dearimranullah <107716481+Dearimranullah@users.noreply.github.com>
2023-05-31 10:48:59 +0500

    Initial Commit

---
+++ LOgin/LOginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOginApp.swift
+//  LOgin
+//
+//  Created by Apple1 on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct LOginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
